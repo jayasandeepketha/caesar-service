0b46b42009708281aebe8f4c8e68df220a3d3922
jayasandeepketha <159554759+jayasandeepketha@users.noreply.github.com>
2024-02-29 15:37:03 -0500

    Add files via upload

--- ciphers-service/ciphers_project/api/views.py
+++ ciphers-service/ciphers_project/api/views.py
@@ -1,5 +1,6 @@
 #from django.shortcuts import render
 from django.http import JsonResponse
+from .ciphers import caesar_encode
 
 def greetings(request):
     result = {"message": "Welcome to ciphers service"}
@@ -8,7 +9,7 @@ def greetings(request):
 def encode(request, plaintext, shift):
     parameters = dict(request.GET)
     print(parameters)
-    ciphers = caesar_encode(plaintext, shift)
+    cipher = caesar_encode(plaintext, shift)
     return JsonResponse({"cipher": cipher})
 
 # Create your views here.
